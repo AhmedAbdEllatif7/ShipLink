fc854c54cd0cb2071ab8d4feb5d9751849967434
Ahmed Abd Ellatif <uaahmed89@gmail.com>
2026-04-09 17:03:55 +0200

    feat: add AdminUserSeeder and update DatabaseSeeder with initial user records

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call(AdminUserSeeder::class);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test Merchant',
+            'email' => 'merchant@example.com',
+            'type' => 'merchant',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test Driver',
+            'email' => 'driver@example.com',
+            'type' => 'driver',
         ]);
     }
 }

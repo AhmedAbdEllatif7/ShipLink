6944aab145ad9a5100440d23858db60e257030f4
Ahmed Abd Ellatif <uaahmed89@gmail.com>
2026-04-23 12:32:13 +0200

    feat: initialize welcome view and define web routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    $type = auth()->user()->type->value;
+    return match ($type) {
+        'admin' => redirect()->route('admin.dashboard'),
+        'merchant' => redirect()->route('merchant.dashboard'),
+        'driver' => redirect()->route('driver.dashboard'),
+        default => redirect('/'),
+    };
+})->middleware(['auth'])->name('dashboard');
+
